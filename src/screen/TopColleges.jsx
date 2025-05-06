9cbad7c2955a3aa8ae5d14155006604854f777ed
unknown <ka344057@gmail.com>
2025-05-06 17:32:01 +0530

    tried to implement search functionality

--- src/screen/TopColleges.jsx
+++ src/screen/TopColleges.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import getTopColleges from "../API_CALLS/getTopColleges";
-import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
-import submitEnquiry from "../API_CALLS/submitEnquiry";
 import HoverCard from "../components/HoverCard";
+import { FaSearch } from "react-icons/fa";
 
 export default function TopColleges() {
   const [collegeList, setCollegeList] = useState([]);
   const [isLoaded, setLoaded] = useState(false);
   const [hasError, setHasError] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const search = () => {
+    const found = collegeList.find(
+      (uni) => uni.university_name.toLowerCase() === searchText.toLowerCase()
+    );
+    return found ? "University Found" : "University Not Found";
+  };
 
   useEffect(() => {
     getTopColleges()
@@ -45,10 +52,28 @@ export default function TopColleges() {
       <h2 className="text-3xl  sm:text-4xl font-bold text-center text-blue-700 mb-8">
         Top Colleges in India
       </h2>
-
+      <div>
+        <div className="relative mb-10 w-[30vw]">
+          <input
+            className="px-4 py-2 pl-10 w-full border rounded-md"
+            placeholder="Search for university by name"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              console.log(searchText);
+            }}
+            onClick={(e) => {
+              if (e.target.key === "enter") {
+                console.log(search(searchText));
+              }
+            }}
+          />
+          <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {collegeList.map((data, index) => (
-          <div className="cursor-pinter">
+          <div className="cursor-pinter" key={index}>
             <HoverCard
               key={index}
               image={data.university_img}
